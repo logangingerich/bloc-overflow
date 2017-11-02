680a95e6d99ba15322bd8db3ab11df760f98e362
Logan Gingerich <loganpgingerich@gmail.com>
2017-11-01 19:09:55 -0600

    Adds auth token

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   scope '/api' do
     # resources :somethings
   end
+=======
+  mount_devise_token_auth_for 'User', at: 'auth'
+    scope '/api' do
+      # resources :blocapi
+    end
+>>>>>>> 05fa50f... Adds Devise Auth Token
 end
