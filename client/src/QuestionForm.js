54fc47decaea95f9ad2c1fcdb0856e3c5d569cac
Trenton Greer <yoshigreer@gmail.com>
2017-11-12 19:08:09 -0700

    redirect working properly

--- client/src/QuestionForm.js
+++ client/src/QuestionForm.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { Link, Redirect } from 'react-router-dom'
+import { Link, withRouter, Redirect } from 'react-router-dom'
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
 
 class QuestionForm extends Component {
+
+
   constructor(props) {
       super(props);
+
+      this.state = {
+        redirect: false
+      }
+
 
       // This binding is necessary to make `this` work in the callback
       this.handleClick = this.handleClick.bind(this);
@@ -13,25 +20,29 @@ class QuestionForm extends Component {
 
     handleClick() {
       console.log("button clicked");
+      console.log(this.state.redirect);
 
       axios.post('/api/questions', {
           title: document.getElementById("title").value,
           description: document.getElementById("description").value
         })
-        .then(function (response) {
-          console.log("questions created")
-          console.log(response);
-          //<Redirect from="/question/create" to="/" />;
-          window.location.href="/";
-        })
+        .then(() => this.setState({ redirect: true }),
+        window.alert("question saved")
+      )
         .catch(function (error) {
           console.log(error);
         });
     }
 
   render() {
+    const { redirect } = this.state;
+
+    if (redirect) {
+      return <Redirect to="/" />
+    }
+
     return (
-      <Container>
+        <Container>
         <Container text>
           this is the question page
         </Container>
@@ -40,9 +51,13 @@ class QuestionForm extends Component {
         </Container>
         <div class="ui input">
           <input id="title" type="text" placeholder="Enter question" />
-          <input id="description" type="text" placeholder="Enter details about your question" />
-          <Button onClick={this.handleClick} class="ui button">Submit</Button>
+          <br />
         </div>
+        <div class="ui input">
+          <input id="description" type="text" placeholder="Enter details" />
+          <br />
+        </div>
+        <Button class="ui green button" onClick={this.handleClick}>Submit</Button>
       </Container>
     )
   }
