c5a92de0839fe879bb6c82de3ecde4216a9840f8
Shannon Bertucci <hello@shannon.io>
2017-11-21 23:25:33 -0700

    Updates Homepage HTML and indentation

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import OtherPage from './OtherPage'
-import QuestionForm from './QuestionForm'
-import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import QuestionForm from './QuestionForm';
+import {Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider} from 'semantic-ui-react';
 
 class HomePage extends Component {
   render() {
-    const { questions } = this.props
+    const {questions} = this.props;
     return (
       <Container>
+        <Container text>This is the Home Page</Container>
         <Container text>
-            This is the Home Page
+          <Link to="/question/create">Create a question</Link>
         </Container>
-        <Container text>
-            <Link to='/other'>Click here for Other page</Link>
-        </Container>
-        <Container text>
-          <Link to='/question/create'>Create a question</Link>
-        </Container>
-        {questions.map((question) =>
-          <Container>
+        <Container>
+          {questions.map(question => (
+            <p>
               <h3>{question.title}</h3>
-              <p>{question.description}</p>
-          </Container>
-        )}
+              <span>{question.description}</span>
+            </p>
+          ))}
+        </Container>
       </Container>
-    )
+    );
   }
 }
 
-export default HomePage
+export default HomePage;
