6d375e0d60cc41085e3fa52434075969c7038d49
Shannon Bertucci <hello@shannon.io>
2017-11-21 23:45:23 -0700

    Updates flow and Question form page

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+// @flow
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import HomePage from './components/HomePage';
--- client/src/components/QuestionForm.js
+++ client/src/components/QuestionForm.js
@@ -1,64 +1,70 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Redirect } from 'react-router-dom'
-import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+import {Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider} from 'semantic-ui-react';
 
 class QuestionForm extends Component {
-
-
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.state = { redirect: false }
+    this.state = {redirect: false};
 
-      // This binding is necessary to make `this` work in the callback
-      this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  fetch(endpoint) {
+    return new Promise((resolve, reject) => {
+      window
+        .fetch(endpoint)
+        .then(response => response.json())
+        .then(json => resolve(json))
+        .catch(error => reject(error));
+    });
+  }
+
+  handleClick() {
+    var title = document.getElementById('title').value;
+    var description = document.getElementById('description').value;
+    if (!title) {
+      alert('please enter a question');
+      return;
     }
 
-    handleClick() {
-      var title = document.getElementById("title").value;
-      var description = document.getElementById("description").value;
-      if (!title) {
-        alert("please enter a question");
-        return;
-      }
-
-      axios.post('/api/questions', {
-        title: title,
-        description: description
-      })
-      .then(() => this.setState({ redirect: true }),
-        alert("question saved")
-      )
-      .catch(function (error) {
+    this.fetch('/api/questions', {
+      method: 'POST',
+      body: {title, description},
+    })
+      .then(() => this.setState({redirect: true}), alert('question saved'))
+      .catch(function(error) {
         console.log(error);
       });
-    }
+  }
 
   render() {
-    const { redirect } = this.state;
+    const {redirect} = this.state;
 
     if (redirect) {
-      return <Redirect to="/" />
+      return <Redirect to="/" />;
     }
 
     return (
       <Container>
-        <Container text>
-          What is your question?
-        </Container>
-        <div class="ui input">
+        <h1 className="ui header">What is your question?</h1>
+        <div className="ui input">
           <input id="title" type="text" placeholder="Enter question" />
           <br />
         </div>
-        <div class="ui input">
+        <div className="ui input">
           <input id="description" type="text" placeholder="Enter details" />
           <br />
         </div>
-        <Button class="ui green button" onClick={ this.handleClick }>Submit</Button>
+        <div>
+          <Button className="ui green button" onClick={this.handleClick}>
+            Submit
+          </Button>
+        </div>
       </Container>
-    )
+    );
   }
 }
 
-export default QuestionForm
+export default QuestionForm;
